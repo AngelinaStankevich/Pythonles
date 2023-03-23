2570dc9f1a1c224e3a584e9c2e924d0a4531e7b5
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2023-03-23 22:53:15 +0300

    add new changes into main10

--- main10.py
+++ main10.py
@@ -19,11 +19,13 @@ class Auto:
 
 
 auto = Auto("audi", 3, "black", "a8", 1000)
-auto.move()
-auto.stop()
-print(auto.mark)
+# auto.move()
+# auto.stop()
+# print(auto.mark)
 
 #2 задание
+import time
+
 
 class Truck(Auto):
     def __init__(self, max_load):
@@ -33,7 +35,26 @@ class Truck(Auto):
         print("attantion")
         super().move()
 
+    def load(self):
+        time.sleep(1)
+        print("load")
+        time.sleep(1)
 
 
 class Car(Auto):
-    pass
+    def __init__(self, max_speed):
+        self.max_speed = max_speed
+
+    def move(self):
+        super.move()
+        print(f"max speed is {self.max_speed}")
+
+
+truck = Truck(1000)
+car = Car(280)
+truck.move()
+truck.load()
+print(truck.max_load)
+print(car.max_speed)
+
+
