cb18dbdbdab823e5f129d79a64c21dca08677915
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2023-04-02 19:06:41 +0300

    Исправленная работа

--- main11dz.py
+++ main11dz.py
@@ -2,7 +2,15 @@
 # Добавить методы пополнения баланса и снятия средств с баланса. Класс для истории должен
 # хранить операцию (снятие или пополнение), сумму, дату и время операции.
 # Создать экземпляр банковского аккаунта и проверить его работу.
+
 from datetime import datetime
+
+
+class AccountHistory:
+    def __init__(self, operation, amount, data_time):
+        self.operation = operation
+        self.amount = amount
+        self.data_time = data_time
 
 
 class BankAccount:
@@ -10,35 +18,28 @@ class BankAccount:
         self.balance = 0
         self.history = []
 
-    def add_money(self, amount):  # пополнение счета
+    def add_money(self, amount: int or float) -> int or float:  # пополнение счета
         self.balance += amount
-        operation = {
-            "Тип операции": "пополнение счета",
-            "Сумма": amount,
-            "Дата и время": datetime.now()
-        }
+        operation = AccountHistory("пополнение счета", amount, datetime.now())
         self.history.append(operation)
 
-    def withdraw_money(self, amount):    # снятие денег со счета
+    def withdraw_money(self, amount):  # снятие денег со счета
         if self.balance < amount:
             print("Недостаточно средств")
         else:
             self.balance -= amount
-            operation = {
-                "Тип операции": "снятие денег",
-                "Сумма": amount,
-                "Дата и время": datetime.now()
-            }
+            operation = AccountHistory("снятие денег", amount, datetime.now())
             self.history.append(operation)
 
     def print_history(self):
         for operation in self.history:
-            print("{}: {} on {}".format(operation["Тип операции"], operation["Сумма"], operation["Дата и время"]))
+            print("{}: {} on {}".format(operation.operation, operation.amount, operation.data_time))
 
 
 account = BankAccount()
 account.add_money(1000)
 account.withdraw_money(200)
 account.withdraw_money(350)
+account.add_money(10000)
+account.withdraw_money(500)
 account.print_history()
-
